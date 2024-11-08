ec778b2382adf3e4faf458c05dc660ab2b287e3c
Giacomo Lanzi <50149567+giacomolanzi@users.noreply.github.com>
2024-11-08 09:50:08 +0200

    Update planbproject-dove-mi-trovo.php

--- planbproject-dove-mi-trovo.php
+++ planbproject-dove-mi-trovo.php
@@ -1,14 +1,14 @@
 <?php
 
-/*
-Plugin Name: "Dove mi trovo?" shortcodes
-Description: Aggiunge i campi opzione personalizzati "dove mi trovo", "Aggiornato al" e "Distanza da casa" usabili con shortcode
-Version: 0.2
-Author: Giacomo Lanzi
-Plugin URI: https://github.com/giacomolanzi/planbproject-dove-mi-trovo
-GitHub Plugin URI: giacomolanzi/planbproject-dove-mi-trovo
-GitHub Branch: main
-*/
+/**
+ * Plugin Name: "Dove mi trovo?" shortcodes
+ * Description: Aggiunge i campi opzione personalizzati "dove mi trovo", "Aggiornato al" e "Distanza da casa" usabili con shortcode
+ * Version: 0.2
+ * Author: Giacomo Lanzi
+ * Plugin URI: https://github.com/giacomolanzi/planbproject-dove-mi-trovo
+ * GitHub Plugin URI: giacomolanzi/planbproject-dove-mi-trovo
+ * GitHub Branch: main
+ */
 
 // Registra i nuovi endpoint
 add_action('rest_api_init', function () {
